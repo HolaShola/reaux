66049899fb0fa3a52ad8631af93682db55bed58f
HolaShola <xxjxxx@mail.ru>
2018-02-02 13:28:49 +0300

    add example with except

---
+++ src/doc/Chapter_10.py
@@ -0,0 +1,10 @@
+while True:
+    reply = input('Enter text: ')
+    if reply == 'stop':
+        break
+    elif not reply.isdigit():
+        print('Bad! ' * 3)
+    else:
+        print(int(reply) ** 2)
+print('Bye')
+    

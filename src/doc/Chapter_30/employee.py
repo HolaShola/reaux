279ca823569e6a10eee75597bd1ac19290936957
HolaShola <xxjxxx@mail.ru>
2018-02-12 15:56:40 +0300

    chapter_30 next commit

--- src/doc/Chapter_30/employee.py
+++ src/doc/Chapter_30/employee.py
@@ -7,9 +7,37 @@ class Employee:
         self.salary = self.salary + (self.salary * percent)
 
     def work(self):
-        print self.name, "does stuff"
+        print(self.name, 'does stuff')
 
     def __repr__(self):
         return "<Employee: name = %s, salary = %s>" % (self.name, self.salary)
     
+class Chef(Employee):
+    def __init__(self, name):
+        Employee.__init__(self, name, 50000)
     
+    def work(self):
+        print(self.name, 'makes food')
+
+class Server(Employee):
+    def __init__(self, name):
+        Employee.__init__(self, name, 40000)
+
+    def work(self):
+        print(self.name, 'interfaces with customer')
+
+class PizzaRobot(Chef):
+    def __init__(self, name):
+        Chef.__init__(self, name)
+
+    def work(self):
+        print(self.name, 'makes pizza')
+
+if __name__ == '__main__':
+    bob = PizzaRobot('Bob')
+    print(bob)   # Вызвать унаследованный метод __repr__
+    bob.work()
+
+    for klass in Employee, Chef, Server, PizzaRobot:
+        obj = klass(klass.__name__)
+        obj.work()

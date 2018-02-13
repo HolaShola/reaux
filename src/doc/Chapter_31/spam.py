490857ea222c2038db1a65fc273f14ab9db77729
HolaShola <xxjxxx@mail.ru>
2018-02-13 17:42:35 +0300

    add chapter_31 next commit

---
+++ src/doc/Chapter_31/spam.py
@@ -0,0 +1,8 @@
+class Spam:
+    numInstances = 0
+
+    def __init__(self):
+        Spam.numInstances = Spam.numInstances + 1
+
+    def printNumInstances():
+        print("Number of instances: ", Spam.numInstances)

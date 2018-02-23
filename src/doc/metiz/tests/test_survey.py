adbb4883573dbb7038b7fc492a0bc19e8e6e9684
HolaShola <xxjxxx@mail.ru>
2018-02-23 13:44:53 +0300

    add setUp method

--- src/doc/metiz/tests/test_survey.py
+++ src/doc/metiz/tests/test_survey.py
@@ -10,5 +10,13 @@ class TestAnonymousSurvey(unittest.TestCase):
         self.assertIn('English', my_survey.responses)
 
     def test_store_three_responses(self):
+        question = "What language did you first learn to speak?"
+        my_survey = AnonymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
 
 unittest.main()

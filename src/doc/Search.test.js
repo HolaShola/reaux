db6d43e660e2f88481aa53e7ca6a746a12cc030c
Yauheni_Sholakhau <Yauheni_Sholakhau@epam.com>
2018-03-14 14:26:56 +0300

    add new functions for test

--- src/doc/Search.test.js
+++ src/doc/Search.test.js
@@ -40,8 +40,73 @@ describe('Search component', () => {
   it('onSelect method should work correctly', () => {
     const wrapper = mount(<Search.WrappedComponent {...defaultProps} />);
     wrapper.instance().onSelect('test_value', { id: 100 });
+
     expect(wrapper.instance().state).toHaveProperty('searchString', '');
     expect(defaultProps.openUnitInfoSidebar).toHaveBeenCalled();
   });
 
+  it('renderItem method should work correctly', () => {
+    const wrapper = mount(<Search.WrappedComponent {...defaultProps} />);
+    const spy = sinon.spy(wrapper.instance(), 'renderItem');
+
+    wrapper.instance().renderItem([], true);
+    expect(spy.calledOnce).toEqual(true);
+  });
+
+  it('sortItems method should work correctly', () => {
+    const wrapper = mount(<Search.WrappedComponent {...defaultProps} />);
+    const spy = sinon.spy(wrapper.instance(), 'sortItems');
+
+    const itemA = { name: 'First' };
+    const itemB = { name: 'Second' };
+
+    wrapper.instance().sortItems(itemA, itemB, 'test');
+    expect(spy.calledOnce).toEqual(true);
+  });
+
+  // it('renderItems method should work correctly', () => {
+  //   const wrapper = mount(<Search.WrappedComponent {...defaultProps} />);
+  //   const spy = sinon.spy(wrapper.instance(), 'renderItems');
+
+  //   wrapper.instance().renderItems([]);
+  //   expect(spy.calledOnce).toEqual(true);
+  // });
+
+  it('renderItems method (items.length = 0) should work correctly', () => {
+    const wrapper = mount(<Search.WrappedComponent {...defaultProps} />);
+    const spy = sinon.spy(wrapper.instance(), 'renderItems');
+
+    wrapper.instance().renderItems([]);
+    expect(spy.returnValues[0]).toEqual(<div
+      className="renderAutocompleteList noResult"
+      style={{ paddingLeft: '10px', width: '248px', paddingTop: '5px' }}
+    >No results
+    </div>);
+  });
+
+  it('renderItems method (items.length > 0) should work correctly', () => {
+    const wrapper = mount(<Search.WrappedComponent {...defaultProps} />);
+    const spy = sinon.spy(wrapper.instance(), 'renderItems');
+
+    const itemA = 'First';
+
+    wrapper.instance().renderItems(itemA);
+    expect(spy.returnValues[0]).toEqual(<div
+      className="renderAutocompleteList"
+    >First
+    </div>);
+  });
+
+  // it('renderItems method (items.length > 7) should work correctly', () => {
+  //   const wrapper = mount(<Search.WrappedComponent {...defaultProps} />);
+  //   const spy = sinon.spy(wrapper.instance(), 'renderItems');
+
+  //   const itemA = 'verylongstring';
+
+  //   wrapper.instance().renderItems([itemA]);
+  //   expect(spy.returnValues[0]).toEqual(<div
+  //     className="renderAutocompleteList"
+  //   >First
+  //   </div>);
+  // });
 });

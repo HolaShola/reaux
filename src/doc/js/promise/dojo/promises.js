d77c2a8f3c597a1184244854d206918407481fee
HolaShola <xxjxxx@mail.ru>
2018-05-08 13:05:49 +0300

    dojo promises next commit

--- src/doc/js/promise/dojo/promises.js
+++ src/doc/js/promise/dojo/promises.js
@@ -1,12 +1,57 @@
-function applyForVisa(documents, resolve, reject) {
+// function applyForVisa(documents, resolve, reject) {
+//     console.log('Обработка заявления...');
+//     setTimeout(() => {
+//         Math.random() > 0.5 ? resolve({}) : reject('В визе отказано!');
+//     }, 2000);
+// }
+
+function applyForVisa(documents) {
     console.log('Обработка заявления...');
-    setTimeout(() => {
-        Math.random() > 0.5 ? resolve({}) : reject('В визе отказано!');
-    }, 2000);
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            Math.random() > 0.5 ? resolve({}) : reject('В визе отказано!');
+        }, 2000);
+    });
+    return promise;
 }
 
-applyForVisa(
-    {},
-    visa => console.info('Виза получена'),
-    (reason) => console.error(reason)
-);
+function getVisa(visa) {
+    console.info('Виза получена');
+    return visa;
+}
+
+function bookHotel(visa) {
+    console.log(visa);
+    console.log('Бронируем отель');
+    return {};
+}
+
+function buyTickets(booking) {
+    console.log('Покупаем билеты');
+    console.log('Бронь', booking);
+}
+
+applyForVisa({})
+    .then(getVisa)
+    .then(bookHotel)
+    .then(buyTickets)
+    .catch(error => console.error(error))
+
+// applyForVisa(
+//     {},
+//     visa => {
+//         console.info('Виза получена');
+//         bookHotel(
+//             visa,
+//             (reservation) => {
+//                 buyTickets(
+//                     reservation,
+//                     () => {},
+//                     () => {}
+//                 )
+//             },
+//             (error) => {}
+//         );
+//     },
+//     (reason) => console.error(reason)
+// );

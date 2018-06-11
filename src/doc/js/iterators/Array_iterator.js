d872085ec7f94dfd095a47dab72e9d6ecd933963
HolaShola <xxjxxx@mail.ru>
2018-06-11 14:05:56 +0300

    add examples for iterator's methods

--- src/doc/js/iterators/Array_iterator.js
+++ src/doc/js/iterators/Array_iterator.js
@@ -4,4 +4,4 @@ const itrObj = priority[Symbol.iterator]();
 itrObj.next();
 itrObj.next();
 itrObj.next();
-console.log(itrObj.next());  // {value: undefined, done: true}  
+console.log(itrObj.next());  // {value: undefined, done: true}      

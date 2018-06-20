2f5b629f6afbb71a02dda0efcb56466389f6748e
HolaShola <xxjxxx@mail.ru>
2018-06-20 17:18:58 +0300

    add new examples for promises

---
+++ src/doc/js/promise/promise_main/examples/error_handling/7.js
@@ -0,0 +1,15 @@
+function doAsync() {
+    return Promise.resolve('OK');
+}
+
+doAsync()
+    .then(
+        value => {
+            console.log(`Resolved! ${value}`);
+            throw new Error('Error!!!');
+        },
+        reason => {
+            console.log(`Rejected! ${reason}`);
+        }
+    );
+    

bed5ffbab11f96836092f6807d800bc7b3dfc5b9
HolaShola <xxjxxx@mail.ru>
2018-06-21 12:40:50 +0300

    add Promise.race example

--- src/doc/js/promise/promise_main/examples/parallel/20.js
+++ src/doc/js/promise/promise_main/examples/parallel/20.js
@@ -9,13 +9,13 @@ function doAsync1() {
 function doAsync2() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(2);
+            reject(2);
         }, 5000);
     });
 }
 
 Promise.all([doAsync1(), doAsync2()])
     .then(
-        val => console.log(val[0] + val[1]),
+        val => console.log('Success'),
         reason => console.log(`Error: ${reason}`)
     );

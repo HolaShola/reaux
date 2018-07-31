fb9b62d904062f92259bf7813c63dc9ebab25f81
HolaShola <xxjxxx@mail.ru>
2018-07-31 16:21:53 +0300

    problem with setState fixed

--- src/doc/react/mainApp/app/components/App.jsx
+++ src/doc/react/mainApp/app/components/App.jsx
@@ -6,48 +6,42 @@ import Pagination from './Pagination/Pagination';
 
 class App extends Component {
   constructor(props) {
-    console.log('constructor');
     super(props);
+    console.log('constructor');
     this.state = { pageNumber: 1 };
     this.childListWrapper = React.createRef();
   }
 
-  componentWillMount() {
-    
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps');
+    return null;
   }
 
   componentDidMount() {
-    
-  }
-
-  fetchData = () => {
+    console.log('componentDidMount');
     const { dispatch } = this.props;
-    this.setState((prevState) => ({
-      pageNumber: prevState.pageNumber++
-    }));
-    
     dispatch(fetchMyData(this.state.pageNumber));
-  };
+    this.setState((prevState) => {
+      return { pageNumber: prevState.pageNumber + 1 }
+    });
+  }
 
   onWheelling = (event) => {
     const { dispatch } = this.props;
-    // Высота экрана
+    // display height
     const displayHeight = document.documentElement.clientHeight;
     const delta = event.deltaY;
     if (event.preventDefault) event.preventDefault();
       if (Math.abs(delta) > 3) {
-      // this.childListWrapper.current.scrollTop += delta / 10;
-        window.scrollBy(0, delta / 10);
+        window.scrollBy(0, delta / 6);
       }
       // for firefox event.deltaY = 3
-      //this.listRef.scrollTop += delta * 4;
-  
+    
     if (this.childListWrapper.current.scrollHeight - displayHeight - window.scrollY < 210) {
       dispatch(fetchMyData(this.state.pageNumber));
-      this.setState({ pageNumber: this.state.pageNumber++ });
-      // this.setState((prevState) => ({
-      //   pageNumber: prevState.pageNumber++
-      // }));
+      this.setState((prevState) => {
+        return { pageNumber: prevState.pageNumber + 1 }
+      });
     }
   }
 
@@ -58,7 +52,6 @@ class App extends Component {
         onWheel={this.onWheelling}
         ref={this.childListWrapper}  
       >
-        <button onClick={this.fetchData}>Fetch</button>
         <Tree films={this.props.films} />
         {/* <Pagination total_pages={this.props.total_pages} dispatch={this.props.dispatch}/> */}
       </div>
